7e10aef63d16c7571f149d00b99cdf8b93545ba8
BoDee <nyx0hemir@gmail.com>
2021-05-05 00:56:40 -0600

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?")
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 var enemyName = "Roborto";
 var enemyHealth = 50;
@@ -30,12 +31,17 @@ var fight = function() {
 
         if (enemyHealth <= 0) {
             window.alert(enemyName + " has died!");
-        }
-        else {
+        } else {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
     } else if (promptFight === "skip" || promptFight === "SKIP") {
-        window.alert(playerName + " has chosen to skip the fight!");
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+        if(confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            playerMoney = playerMoney - 2;
+        } else {
+            fight();
+        }
     } else {
         window.alert("You need to choose a valid option. Try again!")
     }
